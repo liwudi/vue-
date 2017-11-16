3a3222e2b15a26314ef4dae91c4aa15ac6552194
liufang <liufang@mapbar.com>
2017-11-16 17:09:50 +0800

    Merge remote-tracking branch 'remotes/origin/master' into liufang
    # Conflicts:
    #	src/components/sidebar.vue
    #	src/router/index.js

--- src/components/sidebar/SidebarConfig.js
+++ src/components/sidebar/SidebarConfig.js
@@ -11,7 +11,7 @@ const menuData = [
   {title: '流量管理', index: '2', icon: icon},
   {title: '账单查询', index: 'Reconciliation', icon: icon},
   {title: '供应商管理', index: '4', icon: icon},
-  {title: '渠道管理', index: '5', icon: icon},
+  {title: '渠道管理', index: 'DistributorManage', icon: icon},
   {title: '用户管理',index: 'UserManagement',  icon: icon},
   {title: '模板',index: 'basetable',  icon: icon}
 ];
