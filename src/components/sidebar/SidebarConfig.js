08c2b1f1196c3d2281882a7678ba090438c4b910
liufang <liufang@mapbar.com>
2017-11-16 17:24:55 +0800

    Merge remote-tracking branch 'remotes/origin/gaohp' into liufang
    # Conflicts:
    #	src/components/sidebar.vue
    #	src/router/index.js

--- src/components/sidebar/SidebarConfig.js
+++ src/components/sidebar/SidebarConfig.js
@@ -8,7 +8,7 @@ const menuData = [
     {title: '可选商品', index: '1-2', icon: ''},
     {title: '供应商品', index: '1-3', icon: ''},
   ]*/},
-  {title: '流量管理', index: '2', icon: icon},
+  {title: 'SIM卡信息管理', index: 'SimManage', icon: icon},
   {title: '账单查询', index: 'Reconciliation', icon: icon},
   {title: '供应商管理', index: '4', icon: icon},
   {title: '渠道管理', index: 'DistributorManage', icon: icon},
