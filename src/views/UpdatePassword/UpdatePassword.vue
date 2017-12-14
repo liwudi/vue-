9a1bf0de642787ac81c835d37b51e8ce2ba822f0
wangxin2 <wangxin2@mapbar.com>
2017-12-14 16:59:32 +0800

    添加修改密码验证

--- src/views/UpdatePassword/UpdatePassword.vue
+++ src/views/UpdatePassword/UpdatePassword.vue
@@ -1,13 +1,13 @@
 <template>
   <el-main>
-    <el-form ref="updatePasswordForm"   :model="updatePasswordForm" label-width="100px" size="medium">
+    <el-form ref="updatePasswordForm" :rules="passwordValidate"   :model="updatePasswordForm" label-width="100px" size="medium">
       <el-form-item label="旧密码" prop="oldPassword">
         <el-input v-model="updatePasswordForm.oldPassword" placeholder="请输入旧密码"></el-input>
       </el-form-item>
       <el-form-item label="新密码" prop="newPassword">
         <el-input v-model="updatePasswordForm.newPassword" placeholder="请输入新密码"></el-input>
       </el-form-item>
-      <el-form-item label="确认新密码" >
+      <el-form-item label="确认新密码" prop="qnewPassword">
         <el-input v-model="updatePasswordForm.qnewPassword" placeholder="请再次输入新密码"></el-input>
       </el-form-item>
 
@@ -29,17 +29,40 @@
 
   export default {
     data() {
+      const valideRePassword = (rule, value, callback) => {
+        if (value !== this.updatePasswordForm.newPassword) {
+          callback(new Error('两次输入密码不一致'));
+        } else {
+          callback();
+        }
+      };
       return {
         formName: 'updatePasswordForm',
         updatePasswordForm: {
-          userId:1,
+          userId:'',
           oldPassword: '',
           newPassword: '',
           qnewPassword: '',
-        }
+        },
+        passwordValidate: {
+          oldPassword: [
+            { required: true, message: '请输入旧密码', trigger: 'blur' }
+          ],
+          newPassword: [
+            { required: true, message: '请输入新密码', trigger: 'blur' },
+            { min: 4, message: '请至少输入4个字符', trigger: 'blur' },
+            { max: 20, message: '最多输入20个字符', trigger: 'blur' }
+          ],
+          qnewPassword: [
+            { required: true, message: '请再次输入新密码', trigger: 'blur' },
+            { validator: valideRePassword, trigger: 'blur' }
+          ]
+        },
       }
     },
-
+    mounted () {
+        this.updatePasswordForm.userId = commonService.getUserInfo().id
+    },
     methods: {
       request() {
         let params = this.$data.updatePasswordForm;
